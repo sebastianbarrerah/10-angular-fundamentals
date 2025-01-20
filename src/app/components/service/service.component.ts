6218201415247024eeb4bcbf69051de5c1520e17
Sebastian barrera <sebasbh2008@hotmail.com>
2025-01-20 15:49:33 -0500

    feat: icons styles

--- src/app/components/service/service.component.ts
+++ src/app/components/service/service.component.ts
@@ -4,8 +4,35 @@ import { Component } from '@angular/core';
   selector: 'app-service',
   imports: [],
   templateUrl: './service.component.html',
-  styleUrl: './service.component.scss'
+  styleUrl: './service.component.scss',
 })
 export class ServiceComponent {
-
+  services = [
+    {
+      id: 'servicios-wiki',
+      name: 'Wikis',
+      icon: '../../../public/icon/wiki.PNG',
+    },
+    {
+      id: 'servicios-docs',
+      name: 'Docs',
+      icon: '../../../public/icon/Doc.PNG',
+    },
+    {
+      id: 'servicios-projects',
+      name: 'Projects',
+      icon: '../../../public/icon/project.svg',
+    },
+    { id: 'servicios-ai', name: 'Ai', icon: '../../../public/icon/face.PNG' },
+    {
+      id: 'servicios-calendar',
+      name: 'Calendar',
+      icon: '../../../public/icon/calendar.PNG',
+    },
+    {
+      id: 'servicios-sites',
+      name: 'Sites',
+      icon: '../../../public/icon/sites.PNG',
+    },
+  ];
 }
