a2b40256b9a3d933bd6bca8536caaf91209e16b2
Sebastian barrera <sebasbh2008@hotmail.com>
2025-01-20 17:04:29 -0500

    style: :lipstick: add style

--- src/app/components/feature/feature.component.ts
+++ src/app/components/feature/feature.component.ts
@@ -4,8 +4,58 @@ import { Component } from '@angular/core';
   selector: 'app-feature',
   imports: [],
   templateUrl: './feature.component.html',
-  styleUrl: './feature.component.scss'
+  styleUrl: './feature.component.scss',
 })
 export class FeatureComponent {
-
+  features: Feature[] = [
+    {
+      image: '../../../public/images/docsStacked.webp',
+      name: 'Docs',
+      description: 'Build any page, <br /> communicate any idea.',
+      ariaLabel: 'Explorar documentos',
+    },
+    {
+      image: '../../../public/images/bookFlat.webp',
+      name: 'Wiki',
+      description: 'One home base for all your knowledge.',
+      ariaLabel: 'Explorar wiki',
+    },
+    {
+      image: '../../../public/images/target.webp',
+      name: 'Projects',
+      description: 'Manage any project from beginning to end.',
+      ariaLabel: 'Explorar proyectos',
+    },
+    {
+      video: '../../../public/videos/everything-you-need-ai.mp4',
+      name: 'Notion AI',
+      description: 'Finds what you need. Does what you need.',
+      ariaLabel: 'Explorar Notion AI',
+      image: ''
+    },
+    {
+      image: '../../../public/images/notionCalendarLogo.webp',
+      name: 'Calendar',
+      description: 'See all your commitments in one place.',
+      ariaLabel: 'Explorar calendario',
+    },
+    {
+      image: '../../../public/images/trophy.webp',
+      name: 'Goals',
+      description: "Track progress toward <br />what's most important.",
+      ariaLabel: 'Explorar logros',
+    },
+    {
+      image: '../../../public/images/globe.webp',
+      name: 'Sites',
+      description: 'Make any page a website in minutes.',
+      ariaLabel: 'Explorar sitios web',
+    },
+    {
+      image: '../../../public/images/shapes1.webp',
+      name: 'Templates',
+      description: 'Get started with one of 30,000+ templates.',
+      ariaLabel: 'Explorar plantillas',
+    },
+  ];
 }
---
+++ src/app/interfaces/feature.ts
@@ -0,0 +1,7 @@
+interface Feature {
+  image: string;
+  video?: string;
+  name: string;
+  description: string;
+  ariaLabel: string;
+}
